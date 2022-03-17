c92535eec63ee4a67afc2fc0a88fccf3d26b97a1
Miriam Feder <mirfeder@gmail.com>
2022-03-16 23:30:17 -0400

    MVP

--- main.js
+++ main.js
@@ -7,36 +7,26 @@ console.log(`OUR EXTENSION WORKS`)
 
     // // Grab page name, first paragraph of page, and all links contained in the article 
     const header = document.querySelector('.main-page-content');
-    // const pageName = header.childNodes[0].innerHTML
-    // const shortText = header.childNodes[1].childNodes[0].innerHTML
     const links = header.querySelectorAll('a');
-    // const dialog = document.createElement('dialog');
-    // const content = document.createElement('p');
-    // content.innerHTML = shortText;
-    // dialog.appendChild(content);
-    // // console.log(link.href)
-    // // 
-    
-
-    // console.log(shortText)
-    // console.log(links);
+  
 
 // For all links, run our function (function being loading the page and displaying header and paragraph)
     links.forEach((link, i) => {
-        link.onmouseover =  () => {
+        link.onmouseover =  (e) => {
             // Create popup box
             const box = document.createElement('div');
             
-            box.style.height = `300px`;
-            box.style.width = '400px';
-            box.style.overflow = 'auto';
-            box.style.backgroundColor = 'pink';
+            var x = e.clientX - 200;
+            var y = e.clientY + 10;
+            box.style.top = `${y}px`;
+            box.style.left = `${x}px`;
+            box.style.boxSizing = `border-box`
+            box.style.maxWidth = `400px`
+            box.style.backgroundColor = '#ecd9fe';
             box.style.position = 'fixed';
-            box.style.top = '400px';
-            box.style.left = '400px';
             box.style.padding = '5px';
             box.style.outline = `1px solid black`
-            
+            console.log(box.style.top, box.style.left)
             box.id = `popupbox`;
             // Add contents of webpage to popupbox
             const tbd = fetch(`${link.href}`)
@@ -44,28 +34,23 @@ console.log(`OUR EXTENSION WORKS`)
                 .then(response => {
                     const doc = new DOMParser();
                     const result = doc.parseFromString(response, 'text/html');
-
-                    
                     const header = result.querySelector('.main-page-content');
                     const pageName = header.childNodes[0].innerHTML
                     const shortText = header.childNodes[1].childNodes[0].innerHTML
-                    // box.innerText = `${pageName}`
                     const pageNm = document.createElement('p');
                     const content = document.createElement('p');
-                    
+            
                     pageNm.style.outline = `1px solid black`
                     pageNm.style.fontSize = 36;
-                    pageNm.style.
+                    pageNm.style.color = `black`
+                    pageNm.style.fontWeight = `bold`;
                     pageNm.style.textAlign = `center`;
-
                     content.style.fontSize = 10;
-                    
-                    
+                    content.style.color = `black`;
                     content.innerHTML = shortText;
                     pageNm.innerHTML = pageName;
                     box.appendChild(pageNm);
                     box.appendChild(content);
-                    console.log(pageNm)
                     }
                 );
             const body = document.querySelector('body');
